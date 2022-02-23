6b37d681a212b08f44181f644735be00f87a1f96
Дорошенко Владимир <dorosenko@MacBook-Pro-doroshenko.local>
2022-02-23 14:55:04 +0300

    revert to simple test

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,6 +2,7 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
@@ -17,66 +18,25 @@ func runeRepeat(forRepeat, count rune) string {
 
 func Unpack(s string) (string, error) {
 	str := []rune(s)
-	var result string
-	var next, escape = false, false
-	if len(str) > 0 && !(unicode.IsLetter(str[0]) || str[0] == []rune(`\`)[0]) {
-		return "", ErrInvalidString
-	}
-	for i, r := range str {
-		switch {
-		case next:
+	var (
+		result strings.Builder
+		next   bool
+	)
+	for i := 0; i < len(str); i++ {
+		if next {
 			next = false
 			continue
-		case !(unicode.IsNumber(r) || unicode.IsLetter(r) || r == []rune(`\`)[0]):
-			return "", ErrInvalidString
-		case r == []rune(`\`)[0] && !escape && i+1 < len(s):
-			escape = true
-			continue
-		case escape && !(r == []rune(`\`)[0] || unicode.IsNumber(r)):
-			return "", ErrInvalidString
-		case i+1 < len(s):
-			if unicode.IsLetter(r) && !unicode.IsNumber(str[i+1]) {
-				result += string(r)
-				continue
-			}
-			if unicode.IsLetter(r) && unicode.IsNumber(str[i+1]) {
-				result += runeRepeat(r, str[i+1])
-				next = true
-				continue
-			}
-			if escape && (unicode.IsNumber(r) || r == []rune(`\`)[0]) && !unicode.IsNumber(str[i+1]) {
-				result += string(r)
-				escape = false
-				continue
-			}
-			if escape && unicode.IsNumber(r) && unicode.IsNumber(str[i+1]) {
-				result += runeRepeat(r, str[i+1])
-				escape = false
-				next = true
-				continue
-			}
-
-			if escape && r == []rune(`\`)[0] && unicode.IsNumber(str[i+1]) {
-				result += runeRepeat(r, str[i+1])
-				escape = false
-				next = true
-				continue
-			}
-
-			if unicode.IsNumber(r) {
-				return "", ErrInvalidString
-
-			}
-
-		default:
-			if escape && r == []rune(`\`)[0] {
-				result += string(r)
-			}
-			if !escape && (unicode.IsNumber(r) || r == []rune(`\`)[0]) {
-				return "", ErrInvalidString
-			}
-			result += string(r)
 		}
+		fmt.Println(i, string(str[i]))
+		if !unicode.IsLetter(str[i]) {
+			return "", ErrInvalidString
+		}
+		if i+1 < len(str) && unicode.IsNumber(str[i+1]) {
+			result.WriteString(runeRepeat(str[i], str[i+1]))
+			next = true
+			continue
+		}
+		result.WriteRune(str[i])
 	}
-	return result, nil
+	return result.String(), nil
 }
